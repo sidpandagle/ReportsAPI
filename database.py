af8d88494bbd3ef200afeeb49bf642b817e90025
sidpandagle <siddhant.pandagle1998@gmail.com>
2022-10-23 00:52:36 +0530

    psycopg2-binary added in requirements.txt

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./report.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./report.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://keiobbkm:Orknb-PfiycWTkC6RX5Vl_cebgDFgOS3@satao.db.elephantsql.com/keiobbkm"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
